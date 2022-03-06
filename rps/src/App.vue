745931fe1f3ad9eb1604af539f5532c0682a2be8
imshubhamcodex <edwssm777@gmail.com>
2022-03-06 11:49:32 +0530

    first commit

---
+++ rps/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+:root {
+  --light-text: #ffffff;
+  --dark-text: hsl(229, 25%, 31%);
+  --score-text: hsl(229, 64%, 46%);
+  --outline: hsl(217, 16%, 45%);
+  --bg-grad-1: hsl(214, 47%, 23%);
+  --bg-grad-2: hsl(237, 49%, 15%);
+  --scissors-1: hsl(39, 89%, 49%);
+  --scissors-2: hsl(40, 84%, 53%);
+  --paper-1: hsl(230, 89%, 62%);
+  --paper-2: hsl(230, 89%, 65%);
+  --rock-1: hsl(349, 71%, 52%);
+  --rock-2: hsl(349, 70%, 56%);
+  --shadow-light: #00000026;
+  --shadow-med: #0000004d;
+}
+body {
+  background: radial-gradient(at top, var(--bg-grad-1), var(--bg-grad-2));
+}
+</style>
---
+++ rps/src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app id="inspire" class="bg-color--primary">
+    <v-system-bar app>
+      <h4 class="heading">Welcome To Arena</h4>
+      <v-spacer></v-spacer>
+
+      <img height="18" src="../assets/icon-rock.svg" alt="Rock" />
+      <img
+        height="18"
+        class="mr-2 ml-2"
+        src="../assets/icon-paper.svg"
+        alt="Paper"
+      />
+      <img height="18" src="../assets/icon-scissors.svg" alt="Scissor" />
+    </v-system-bar>
+
+    <v-app-bar app clipped-right flat height="50" class="top-bar">
+      <h4>ROCK PAPER SCISSOR</h4>
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="156" class="pb-2 pt-2">
+        <v-btn rounded outlined color="teal" dark>
+          Play <v-icon right dark> mdi-motion-play </v-icon></v-btn
+        >
+      </v-responsive>
+    </v-app-bar>
+
+    <!-- <v-navigation-drawer class="aside-nav" app left>
+      <v-list>
+        <v-list-item v-for="n in 5" :key="n" link>
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+
+    <v-main class="bg-color--primary"> This is main </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({ drawer: null }),
+};
+</script>
+
+<style scoped>
+.bg-color--primary {
+  background: radial-gradient(at top, var(--bg-grad-1), var(--bg-grad-2));
+}
+
+/*@media (max-width: 992px) {
+  .aside-nav {
+    width: 100px !important;
+    margin: 0;
+  }
+  .top-bar {
+    left: 0px !important;
+  }
+}*/
+</style>
